1cf7cd6dd46df46410e9fc1d9f24272775728fda
rickybruner <alteredecho07@yahoo.com>
2024-02-22 00:43:24 -0600

    added preliminary login page structure and starting styles

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import styles from './LoginPage.module.css';
+import logo from '../../assets/nimbusLogo.png';
 
 const LoginPage: React.FC = () => {
-    return (
-        <div>
-            Placeholder text for LoginPage buildout.
-            {/* Your login form and related elements here */}
+  return (
+    <div className={styles.loginContainer}>
+        <div className={styles.loginHeader}>
+            <img src={logo} alt='Nimbus Logo' className={styles.logo} />
+            <h1> Welcome to Nimbus</h1>
+            <p>Log in to access the platform</p>
         </div>
-    );
+        <form className={styles.loginForm}>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' placeholder='you@example.com' />
+
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' placeholder='...' />
+
+            <button type='submit' className={styles.loginButton}>Login</button>
+        </form>
+        <div className={styles.loginFooter}>
+            <p>By continuing, you agree to the Terms of Service and Privacy Policy</p>
+            {/* <a href="#">Need Help?</a> */}
+        </div>
+    </div>
+  );
 };
 
 export default LoginPage;
